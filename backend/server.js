0e546904a9d6a0293db7fa79a16b556c4003f6f5
ansil-27 <brightonlinemedia@gmail.com>
2025-07-05 18:24:07 +0530

    to coorect api call in cors

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,9 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: true, // Allow all origins for now
   credentials: true,
 }));
 app.use(express.json()); // Middleware to parse JSON bodies
